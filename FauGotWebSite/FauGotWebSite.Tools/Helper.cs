f356bc029e942b9e77ef3d280d39f545f7e7fa78
john.hernandez <john.m.hernandez@gmail.com>
2017-02-21 23:03:23 -0500

    Created Data Class

---
+++ FauGotWebSite/FauGotWebSite.Tools/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Web;
+
+namespace FauGotWebSite.Tools
+{
+    public class Helper
+    {
+        static public object GetSettingsValue(string configName)
+        {
+            try
+            {
+                return ConfigurationSettings.AppSettings[configName];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        static public void W(string message)
+        {
+            Debug.Write(message);
+            HttpContext.Current.Response.Write("<span style='colo:red;background-color:white;'><b>" + message + "</span><br><br>");
+        }
+        static public void W(string message, Exception ex)
+        {
+            Debug.Write(message);
+            HttpContext.Current.Response.Write("<span style='colo:red;background-color:white;'><b>" + message + ":</b> " + ex.Message + "</span><br><br>");
+        }
+    }
+}
