3df9243ec866278f8c211fc7a0d936680fe0bd1a
john.hernandez <john.m.hernandez@gmail.com>
2017-02-21 23:03:23 -0500

    Created Data Class

---
+++ FauGotWebSite/FauGotWebSite.Data/MySqlStoreProcedure.cs
@@ -0,0 +1,119 @@
+﻿using FauGotWebSite.Tools;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+
+namespace FauGotWebSite.Data
+{
+
+    class MySqlStoreProcedure
+    {
+        MySqlConnection conn;
+        MySqlCommand cmd;
+        DataSet ds;
+
+        public MySqlStoreProcedure(string StoreProcedureName)
+        {
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = Helper.GetSettingsValue("connString").ToString();
+
+                cmd = new MySqlCommand();
+                cmd.CommandText = StoreProcedureName;
+                cmd.CommandType = CommandType.StoredProcedure;
+
+            }
+            catch (MySqlException ex)
+            {
+                Helper.W("Error Connecting to database", ex);
+            }
+            catch (Exception ex)
+            {
+                Helper.W("Exception", ex);
+            }
+            finally
+            {
+                Close();
+            }
+        }
+
+        public void ExecuteDataSet()
+        {
+            MySqlDataAdapter adp = new MySqlDataAdapter();
+            try
+            {
+                ds = new DataSet();
+                Open();
+                adp.SelectCommand = cmd;
+                adp.Fill(ds);
+            }
+            finally
+            {
+                adp.Dispose();
+            }
+        }
+        public void ExecuteNonQuery()
+        {
+            Open();
+            cmd.ExecuteNonQuery();
+        }
+        public object GetOutputValue(string parameterName)
+        {
+            try
+            {
+                return cmd.Parameters["?" + parameterName].Value;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public DataSet GetDataset()
+        {
+            try
+            {
+                if (ds != null)
+                    return ds;
+                else
+                    return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public void AddInputParameter(string parameterName, object parameterValue, MySqlDbType paraemterDataType)
+        {
+            MySqlParameter param = new MySqlParameter("?" + parameterName, paraemterDataType);
+            param.Value = parameterValue;
+            param.Direction = ParameterDirection.Input;
+            cmd.Parameters.Add(param);
+        }
+        public void AddOutputParameter(string parameterName, object parameterValue, MySqlDbType paraemterDataType)
+        {
+            MySqlParameter param = new MySqlParameter("?" + parameterName, paraemterDataType);
+            param.Value = parameterValue;
+            param.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(param);
+        }
+
+        private void Close()
+        {
+            if (conn != null && conn.State != ConnectionState.Closed)
+                conn.Close();
+            conn.Dispose();
+        }
+
+        private void Open()
+        {
+            if (conn != null && conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+        }
+
+
+    }
+}
